b171d8b8eb2e86cd56b97045ed8565ed188b11f7
Shona Wilde <shona.wilde@york.ac.uk>
2021-08-05 14:50:20 +0200

    enhance run regression functions

--- R/do_run_regression.R
+++ R/do_run_regression.R
@@ -1,18 +1,18 @@
 #' Function to perform run regression analysis 
 #'
-#' \code{do_run_regression} calculates the regression results for fixed-width, overlapping windows of data.
+#' \code{do_run_regression} calculates the regression results for fixed-width, overlapping windows of data
 #'
-#' @param df Input data frame.
+#' @param df Input data frame
 #' 
-#' @param formula A formula specifying the model, as in \code{\link[lmodel2]{lmodel2}} and \code{\link[stats]{lm}}.
+#' @param formula A formula specifying the model, as in \code{\link[lmodel2]{lmodel2}} and \code{\link[stats]{lm}}
 #' 
-#' @param n Size of moving window.
+#' @param n Size of moving window
 #' 
 #' @param complete Should the function be evaluated on complete windows only? 
 #' If FALSE then partial computations will be allowed.
 #' 
 #' @param method Regression method as in \code{\link[lmodel2]{lmodel2}}.
-#' Options are ordinary least squares (OLS), major axis (MA), standard major axis (SMA), and ranged major axis (RMA).
+#' Options are ordinary least squares (OLS), major axis (MA), standard major axis (SMA), and ranged major axis (RMA)
 #' 
 #' @param verbose Should messages be displayed to the user?
 #' 
@@ -31,18 +31,19 @@
 #' 
 #' @export
 
+
 do_run_regression <- function(df, formula, n = 5, complete = T, method = "SMA", verbose = T) {
   
   # get and y variables
   formula <- as.formula(formula)
- 
+  
   y <- attr(terms(formula), which = "variable") %>%
     as.character() %>%
     `[`(2)
   
   x <- attr(terms(formula), which = "term.labels")
   
-# check names and class
+  # check names and class
   if (any(!c(x, y) %in% names(df))) {
     stop("Input data must contain both `x` and `y` variables for regression...", call. = FALSE)
   }
@@ -60,37 +61,29 @@ do_run_regression <- function(df, formula, n = 5, complete = T, method = "SMA", 
   
   # add list column containing rolling observations
   df <- df %>% 
-    tibble::rowid_to_column("window_id") %>% 
-    add_rolling_observations(n = n)  %>% 
-    tibble::as_tibble()
+    get_rolling_observations(n = n) 
   
-  # column used as index
-  window_id <- df %>% 
-    pull(window_id)
+  # length of df
+  n_models <- nrow(df)
   
-  # create rolling model objects
-  # wrap to prevent function falling over when lmodel2 encounters an error
-  possible_lmodel2 = purrr::possibly(.f = lmodel2::lmodel2, otherwise = NULL)
-  
-  # apply model to sliding window
-  suppressWarnings(
-    
-    model <- slider::slide_index(
-      df, 
-      window_id, 
-      #  ~lmodel2::lmodel2(formula = formula, data = .x, range.y = "interval", range.x = "interval", nperm = 99),
-      ~possible_lmodel2(formula = formula, data = .x, range.y = "interval", range.x = "interval", nperm = 99),
-      .before = n - 1, 
-      .complete = complete
+  # run models
+  df_model <- df %>% 
+    mutate(
+      model = map2(
+        observations, .$window_id,
+        ~do_run_regression_worker(
+          data = .x, 
+          index = .y, 
+          formula = formula,
+          n_models = n_models, 
+          n = n,
+          complete = complete,
+          verbose = verbose)
+      )
     )
-    
-  )
-  
-  # add to df
-  df$model <- model
   
   # calculate model summaries
-  df_model_stats <- df %>%
+  df_model_stats <- df_model %>%
     filter(!map_lgl(model, is.null)) %>% 
     mutate(
       model_stats = purrr::map(model, shonarrr::tidy_model_summary)
@@ -106,7 +99,6 @@ do_run_regression <- function(df, formula, n = 5, complete = T, method = "SMA", 
       observations,
       model_stats
     ) %>% 
-    mutate(observations = purrr::map(observations, ~select(., -window_id))) %>% 
     tidyr::unnest(observations) %>% 
     rename(y = !!y,
            x = !!x) %>% 
@@ -131,14 +123,63 @@ do_run_regression <- function(df, formula, n = 5, complete = T, method = "SMA", 
     ) %>% 
     distinct_all() %>% 
     ungroup() %>% 
-    group_nest(window_id, .key = "model_stats")
-  
+    group_nest(window_id, .key = "model_stats") %>% 
+    mutate(
+      model_stats = map(model_stats, as_tibble)
+    )
+    
   # overwrite model stats column
   df_model_stats$model_stats <- df_lengths$model_stats
   
   
   return(df_model_stats)
+
   
 }
+
+
+
+do_run_regression_worker <- function(data, index, formula, n, n_models, complete, verbose){
+  
+  # get length of data
+  nrow_window <- nrow(data)
+  
+  # only display message for every 10th model
+  if (verbose && index%%10 == 0) {
+    
+    message(
+      str_date_formatted(), ": Modelling ", index, 
+      " of ", n_models, " times..."
+    )
+  }
+  
+  
+  # wrap to prevent function falling over when lmodel2 encounters an error
+  possible_lmodel2 <-  purrr::possibly(.f = lmodel2::lmodel2, otherwise = NULL)
+  
+  model <- suppressWarnings(
+    
+    possible_lmodel2(
+      formula = formula,
+      data = data, 
+      range.y = "interval",
+      range.x = "interval", 
+      nperm = 99)
+    
+  )
+  
+  # invalidate models where window is smaller than n
+  if (complete){
+    
+    if(nrow_window < n)
+      
+      model <- NULL
+  }
+  
+  return(model)
+  
+}
+
+
 
 
