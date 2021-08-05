e7f5d19ba9f01e087ff628f4d9c8573c176e697b
Shona Wilde <shona.wilde@york.ac.uk>
2021-08-05 16:53:19 +0200

    correct function name call

--- R/calculate_regression_line_length.R
+++ R/calculate_regression_line_length.R
@@ -1,30 +1,30 @@
 #' Function to calculate the length of a regression line
-#' 
+#'
 #' @param df Input data frame. Must contain a slope variable.
 #'
 #' @param x x variable for regression.
-#' 
+#'
 #' @param y y variable for regression.
-#' 
+#'
 #' @return Tibble.
-#' 
+#'
 #' @author Shona Wilde
-#' 
+#'
 #' @export
 
 
 calculate_regression_line_length <- function(df, x, y){
-  
+
   # find min and max x values
-  df <- df %>% 
+  df <- df %>%
     mutate(
       across(x, list(min = min, max = max)
       )
     )
-  
+
   # calculate lengths
   # need some extra logic if slope is negative
-  df_length <- df %>% 
+  df_length <- df %>%
     mutate(
       y_min = (slope*x_min) + intercept,
       y_max = (slope*x_max) + intercept,
@@ -34,12 +34,12 @@ calculate_regression_line_length <- function(df, x, y){
       y_length = if_else(
         y_min < y_max, y_max-y_min, y_min-y_max
       ),
-      length = calc_hypotenuse_length(x_length, y_length)
-    ) %>% 
+      length = calculate_hypotenuse_length(x_length, y_length)
+    ) %>%
     as_tibble()
-  
+
   return(df_length)
-  
+
 }
 
 
