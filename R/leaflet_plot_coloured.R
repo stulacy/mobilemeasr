6ee496060ade4fee63774670df77b9eddd9c7770
Shona Wilde <swilde@nas-10-240-178-13.york.ac.uk>
2021-11-01 14:31:07 +0000

    add leaflet_plot_colourred.R

--- R/leaflet_plot_coloured.R
+++ R/leaflet_plot_coloured.R
@@ -1,53 +1,78 @@
+#' Function to plot coloured geometries on a leaflet map
+#'
+#' This function is a wrapper around \code{\link[gissr]{leaflet_plot}} that allows geometries to be coloured according to a variable.
+#' Discrete and continuous scales are supported.
+#'
+#' @param st Object with class \code{sf} or \code{sfc}.
+#'
+#' @param colour_by Variable to colour geometries by.
+#'
+#' @param popup Vector of variables to be used as a pop-up on the map.
+#'
+#' @param opacity Stroke opacity (edge of geometry).
+#'
+#' @param fill_opacity Fill opacity (inside of geometry).
+#'
+#' @return Leaflet map.
+#'
+#' @author Shona Wilde
+#'
+#' @seealso \code{\link[gissr]{leaflet_plot}}
+#'
+#' @export
 
-leaflet_plot_coloured <- function(st, colour_by = "value", fill_opacity = 0.5) {
-  
-  
+
+leaflet_plot_coloured <- function(st, colour_by = "value",  popup = NULL, opacity = 0.5, fill_opacity = 0.2) {
+
+
   colour_var <- sym(colour_by)
-  
-  domain <- st %>% 
+
+  domain <- st %>%
     pull(!!colour_var)
- 
-  
+
+
   # set palette  based on continuous or discrete variables
-  if (class(domain) %in% c("integer", "numeric")) 
-    
+  if (class(domain) %in% c("integer", "numeric"))
+
   {
     pal <- colorNumeric(
       palette = "plasma",
       domain = domain
     )
-    
+
   }
-  
+
   else {
-    
+
     pal <- colorFactor(
       "plasma",
-      domain = domain, 
+      domain = domain,
       na.color = "black"
     )
-    
+
   }
-  
+
   # build plot with legend
   plot <- gissr::leaflet_plot(
     st,
     fill_opacity = fill_opacity,
-    colour = ~pal(domain)
-  )  %>% 
+    colour = ~pal(domain),
+    popup = popup,
+    opacity = opacity
+  )  %>%
     addLegend(
-      "bottomright", 
+      "bottomright",
       pal = pal,
       values = ~domain,
       title = colour_by,
       opacity = 1
     )
-  
-  
+
+
   return(plot)
 
 }
-  
+
 
 
 
