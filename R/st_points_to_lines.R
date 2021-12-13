611c0cd30fffcb49c5d1b8a64c2ad7d9262c0ec1
shonawilde <shona.wilde@york.ac.uk>
2021-12-13 15:04:22 +0000

    add st_points_to_lines.R

---
+++ R/st_points_to_lines.R
@@ -0,0 +1,41 @@
+#' St points to lines
+#'
+#' Wrapper to cast a spatial points object to lines
+#'
+#' @param st Object with class \code{sf} or \code{sfc}. The geometry type must be 'POINTS'
+#'
+#' @param group_by Optional grouping variable
+#'
+#' @param do_union Should geometries be created by unioning using st_union, or simply by combining using st_combine?
+#'
+#' @author Shona Wilde
+#'
+#' @return Sf object with LINESTRING geometries
+#'
+#' @export
+
+st_points_to_lines <- function(st, group_by = NA, do_union = F) {
+
+
+  if(!is.na(group_by)) {
+
+    group_by <- sym(group_by)
+
+    st_lines <- st %>%
+      group_by(!!group_by) %>%
+      summarise(do_union = do_union) %>%
+      sf::st_cast("LINESTRING")
+
+  }
+
+  else {
+
+    st_lines <- st %>%
+      summarise(do_union = do_union) %>%
+      sf::st_cast("LINESTRING")
+
+  }
+
+  return(st_lines)
+
+}
