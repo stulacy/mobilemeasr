73725a19f6ab098bb84769c87d15842e995099a9
shonawilde <shona.wilde@york.ac.uk>
2023-06-02 10:18:07 +0100

    add drop_empty argument

--- R/st_from_df.R
+++ R/st_from_df.R
@@ -6,17 +6,19 @@
 #'
 #' @param longitude Longitude variable in df
 #'
+#' @param remove_empty Should rows with missing geometries be removed? Default is FALSE.
+#'
 #' @param crs Coordinate reference system of df
 #'
 #' @author Shona Wilde
 #'
-#' @return `sf`object
+#' @return `sf` object
 #'
 #' @export
 
 
 
-st_from_df <- function(df, latitude = "lat", longitude = "long", crs = 4326){
+st_from_df <- function(df, latitude = "lat", longitude = "long", remove_empty = F, crs = 4326){
 
 
   if (any(is.na(c(df[, latitude, drop = T], df[, longitude, drop = T]))))
@@ -41,6 +43,14 @@ st_from_df <- function(df, latitude = "lat", longitude = "long", crs = 4326){
     crs = crs,
     na.fail = F
   )
+
+  if (remove_empty)
+  {
+    st <- st %>%
+      filter(
+        !st_is_empty(geometry)
+      )
+  }
 
   return(st)
 
